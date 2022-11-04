0b1c3ecdafb0dac991f6ace33d7527b1012b0f1e
Mark Konrad <konrad0816@gmail.com>
2022-11-04 20:13:42 +0100

    build(parser): add initial folder structure and makefiles

---
+++ parser/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Parser" << std::endl;
+  return 0;
+}
---
+++ parser/tests/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Parser Test" << std::endl;
+  return 0;
+}
