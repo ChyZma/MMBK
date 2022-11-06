0472598badc7d831a073bdacd448d2feb0995692
Mark Konrad <konrad0816@gmail.com>
2022-11-06 18:37:59 +0100

    test(parser): add temp main.cpp

--- parser/src/main.cpp
+++ parser/src/main.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+#include "caff.hpp"
 
 int main() {
-  std::cout << "Parser" << std::endl;
+  // TODO(mark): this should be some kind of cli tool, but this is probably fine
+  // for testing
+  Caff c = Caff("assets/tests/1.caff");
+  c.parse();
   return 0;
 }
