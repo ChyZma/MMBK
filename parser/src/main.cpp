7e4b488c30a6714267791b3e8db510d9214c89d9
Mark Konrad <konrad0816@gmail.com>
2022-11-08 18:03:46 +0100

    feat(parser): add simple cli tool to main

--- parser/src/main.cpp
+++ parser/src/main.cpp
@@ -1,11 +1,35 @@
 #include "caff.hpp"
+#include <boost/numeric/conversion/detail/meta.hpp>
 
-int main() {
-  // TODO(mark): this should be some kind of cli tool, but this is probably fine
-  // for testing
-  Caff c = Caff("assets/tests/2.caff", "test");
-  c.parse();
-  c.generateGif("test.gif");
-  c.generateMeta("test.ini");
+void help() {
+  std::cout << "Usage: mmbk SOURCE GIF_DEST META_DEST" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc > 0) {
+    if ((std::string)argv[1] == "help") {
+      help();
+      return 0;
+    }
+    if (argc < 3) {
+      help();
+      std::cerr << "CAFF Path, gif destination and meta destination are "
+                   "required arguments"
+                << std::endl;
+      return 1;
+    }
+    std::string caff_path = argv[1];
+    std::string gif_dest = argv[2];
+    std::string meta_dest = argv[3];
+
+    Caff c = Caff(caff_path);
+    c.parse();
+    c.generateGif(gif_dest);
+    c.generateMeta(meta_dest);
+    return 0;
+  } else {
+    help();
+  }
+
   return 0;
 }
