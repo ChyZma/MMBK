f890c4a0eefe81189671e74a7c43b27bca42ea45
Mark Konrad <konrad0816@gmail.com>
2022-11-09 18:24:17 +0100

    test(parser): add tests

--- parser/tests/ciff.cpp
+++ parser/tests/ciff.cpp
@@ -1,9 +1,36 @@
+#include "caff.hpp"
+#include "common_tests.hpp"
 #include <boost/test/unit_test.hpp>
 #include <boost/test/unit_test_suite.hpp>
-#define BOOST_TEST_LOG_LEVEL all
 
 BOOST_AUTO_TEST_SUITE(CIFF_tests)
-BOOST_AUTO_TEST_CASE(ciff_test1) { BOOST_REQUIRE((3 + 3) == 6); }
-BOOST_AUTO_TEST_CASE(ciff_test2) { BOOST_REQUIRE((3 + 3) == 6); }
-BOOST_AUTO_TEST_CASE(ciff_test3) { BOOST_REQUIRE((3 + 3) == 6); }
+BOOST_AUTO_TEST_CASE(ciff_magic_wrong) {
+  Caff c = Caff("../assets/tests/1.caff");
+  c.loadFile();
+  c.bytes[CAFF_SIZES::total * 3 + CAFF_HEADER_SIZES::total + 12 +
+          CAFF_CREDITS_SIZES::total + CAFF_ANIMATION_SIZES::duration] =
+      (byte)'A';
+  u16 ret = c.parse();
+  BOOST_REQUIRE(ret == 41);
+}
+
+BOOST_AUTO_TEST_CASE(ciff_content_size_wrong) {
+  Caff c = Caff("../assets/tests/1.caff");
+  c.loadFile();
+  c.bytes.erase(c.bytes.begin() + CAFF_SIZES::total * 3 +
+                    CAFF_HEADER_SIZES::total + 12 + CAFF_CREDITS_SIZES::total +
+                    CAFF_ANIMATION_SIZES::duration + CIFF_OFFSETS::width,
+                c.bytes.begin() + CAFF_SIZES::total * 3 +
+                    CAFF_HEADER_SIZES::total + 12 + CAFF_CREDITS_SIZES::total +
+                    CAFF_ANIMATION_SIZES::duration + CIFF_OFFSETS::width +
+                    CIFF_SIZES::width);
+  auto wrong_width = intToBytes(1800, 8);
+  c.bytes.insert(c.bytes.begin() + CAFF_SIZES::total * 3 +
+                     CAFF_HEADER_SIZES::total + 12 + CAFF_CREDITS_SIZES::total +
+                     CAFF_ANIMATION_SIZES::duration + CIFF_OFFSETS::width,
+                 wrong_width.begin(), wrong_width.end());
+  u16 ret = c.parse();
+  BOOST_REQUIRE(ret == 42);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
