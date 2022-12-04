e39f216b9b9d36eefd86f0f9dd73e23b826f42a4
Bazsó Márton <bazsomarci98@gmail.com>
2022-12-04 15:16:13 +0100

    update folder structure and add new project

---
+++ backend/CaffBackend/CaffParser/Caff.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CaffParser
+{
+    public class Caff
+    {
+        [DllImport("D:\\school stuff\\BME\\Számítógép-biztonság\\MMBK\\backend\\CaffBackend\\CaffParser\\parser.dll")]
+        private static extern IntPtr Caff_Create(string path);
+        [DllImport("D:\\school stuff\\BME\\Számítógép-biztonság\\MMBK\\backend\\CaffBackend\\CaffParser\\parser.dll")]
+        private static extern UInt16 Caff_Loadfile(IntPtr c);
+        [DllImport("D:\\school stuff\\BME\\Számítógép-biztonság\\MMBK\\backend\\CaffBackend\\CaffParser\\parser.dll")]
+        private static extern UInt16 Caff_Parse(IntPtr c);
+
+        public void Test()
+        {
+            IntPtr Caff = Caff_Create("D:\\school stuff\\BME\\Számítógép-biztonság\\MMBK\\backend\\CaffBackend\\CaffParser\\1.caff");
+            Console.WriteLine(Caff_Loadfile(Caff));
+            Console.WriteLine(Caff_Parse(Caff));
+
+        }
+    }
+}
